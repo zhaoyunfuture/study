fec67753a2c5ea53813dd3c41ecce37b6a300c46
zhaoyunfuture <zyfuture@qq.com>
2019-03-01 19:59:32 +0800

    update reverse_int src

--- leetcode/reverse_int/src/main.cpp
+++ leetcode/reverse_int/src/main.cpp
@@ -1,38 +1,86 @@
 #include <string.h>
 #include <string>
 #include <iostream>
-#include <stdlib.h>
 #include <stack>
+#include <limits.h>
 #include <math.h>
 using namespace std;
-
+#if 0
 class Solution {
 public:
     int reverse(int x) {
-        int i = x;
-        int j = 0;
-        int res = 0;
-        std::stack<int> s;
-        while(!((i%10)&&(i/10%10)))
-        {
-            s.push(i%10);
-            i = i/10;
-        }
-        while(!s.empty())
-        {
-            res += s.top() * pow(10,j);
-            s.pop();
-            j++;
-        }
-        return res;
-    }
+		int i = x;
+		int j = 0;
+		int sum = 0;
+		std::stack<int> s;
+	    unsigned int p =2<<31 -1;
+		if((x>0) && (x > p))
+			return 0;
+		if((x<0))
+		{
+		    unsigned int p = x;
+			unsigned int con = 2<<30;
+			if(p == con)
+				return 0;
+		}
+		while((i%10 != 0) || (i/10 != 0))
+		{
+		    s.push(i%10);
+			i /= 10;
+		}
+		while (!s.empty())
+		{
+		    sum += s.top()* pow(10,j);
+			s.pop();
+			j++;
+		}
+
+		if((sum>0) && (sum > p))
+			return 0;
+		if((x >0) && (sum < 0))
+		    return 0;
+		if(sum <0)
+		{
+			unsigned int p = sum;
+			unsigned int con = 2<<30;
+			if(p == con)
+				return 0;
+		}
+
+        return sum;
+	}
 };
-    
+#endif
+class Solution {
+	public:
+		int reverse(int x) {
+			bool neg = x < 0;
+			if (x == INT_MIN) return 0;
+			if (neg) x = -x;
+			int r = 0;
+			while (x != 0) {
+				if (r > INT_MAX / 10 || r == INT_MAX / 10 && x % 10 > 7)
+					return 0;
+				r = r * 10 + x % 10;
+				x = x / 10;
+			}
+			if (neg) r = -r;
+			return r;
+		}
+};
 int main(int argc, char* argv[])
 {
-  printf("hello \r\n");
-  Solution s;
-  int i = 123;
-  printf("%d",s.reverse(i));
-  return 0;
+    Solution s;
+	cout << s.reverse(-123) << endl;
+	cout << s.reverse(123) << endl;
+	cout << s.reverse(-2147483648) << endl;
+	cout << s.reverse(1534236469) << endl;
+	cout << s.reverse(1463847412) << endl;
+	cout << s.reverse(-1563847412) << endl;
+ cout << "INT_MIN" << INT_MIN <<endl;	
+    return 0;
 }
+
+
+
+
