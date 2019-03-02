bcbc26c58c890a11533d92234366ecd1672ffeab
zhaoyunfuture <zyfuture@qq.com>
2019-03-02 21:35:48 +0800

    update repo,remove useless file

--- Introduction/hello_1.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    cout << "hello world for test" << endl;
-    cout << "test" << endl;
-	return 0;
-}
--- Utility/common.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <sstream>
-#include <stdlib.h>
-
-using namespace std;
-
-bool isnum(string s);
-int stringToInt(string s);
