ddbbb2aead735eae6df70f7e8424a0c3f39d21ac
zhaoyunfuture <zyfuture@qq.com>
2019-04-05 22:38:16 +0800

    finish lowest_common_ancestor_BST, both performance and memory is not so good, but it work out

--- leetcode/lowest_common_ancestor_BST/src/main.cpp
+++ leetcode/lowest_common_ancestor_BST/src/main.cpp
@@ -2,15 +2,16 @@
 #include <string>
 #include <iostream>
 using namespace std;
+
 /**
- ** Definition for a binary tree node.
- ** struct TreeNode {
- **     int val;
- **     TreeNode *left;
- **     TreeNode *right;
- **     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- ** };
- **/
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     vector<TreeNode*> fillPath(TreeNode* root, TreeNode* p) {
@@ -19,21 +20,22 @@ public:
         vector<TreeNode*> r;
         if(root==NULL)
             return v;
-        if(root == p) {
-            v.push_back(p);   
+        //cout << "val = " << root->val << endl;
+        if(root->val == p->val) {
+            v.push_back(root);   
             return v;
         }
         else{
-            l = fillPath(root->left,p);
-            r = fillPath(root->right,p);
+            l = fillPath(root->left,p);           
+            r = fillPath(root->right,p);            
             if(l.size() != 0) {
-                l.push_back(root->left);
+                l.push_back(root);
                 return l;
-            }
-            if(r.size() != 0) {
-                r.push_back(root->right);
-                return r;
             }            
+            else if(r.size() != 0){
+                r.push_back(root);
+                return r;
+            } else return v;           
         }
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -42,20 +44,27 @@ public:
         vector<TreeNode*>::reverse_iterator ritp;
         vector<TreeNode*>::reverse_iterator ritq;
         TreeNode* key=NULL;
-        p_path = fillPath(root,p);
         q_path = fillPath(root,q);
+        p_path = fillPath(root,p);   
+        cout << "q size " << q_path.size() <<endl;
+        cout << "p size " << p_path.size() << endl;
         ritp = p_path.rbegin();
-        ritq = q_path.rbegin();
-        for(;ritp!=p_path.rend();ritp++,ritq++) {
-            if(*ritp == *ritq)
-                key = *ritp;
+        ritq = q_path.rbegin();        
+        int min = q_path.size() > p_path.size()?p_path.size():q_path.size();
+        cout << "min" << min << endl;
+        for(int i=0;i<min;++i) {
+            if(*ritp == *ritq) {
+                key = *ritp;               
+            }
             else
                 break;
+            ritp++;
+            ritq++;
         }
         return key;
-
     }
 };
+
 int main(int argc, char* argv[])
 {
   
