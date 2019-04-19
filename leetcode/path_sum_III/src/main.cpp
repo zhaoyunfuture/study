4cd2e7e12c227fec010a08bcc4dd84b56fe147a6
zhaoyunfuture <zyfuture@qq.com>
2019-04-19 23:14:33 +0800

    path_sum_III, complete, 72%:perf, 100%:mem

--- leetcode/path_sum_III/src/main.cpp
+++ leetcode/path_sum_III/src/main.cpp
@@ -2,42 +2,50 @@
 #include <string>
 #include <iostream>
 using namespace std;
-class Solution {
-public:
-    int ps(TreeNode* root,int sum, int cur){
-        if(!root)
-            return 0;
-        else if(root->val == sum && cur==0)
-            return 1;
-        else if((root->val + cur) == sum)
-            return 1;
-        else{
-            int l = ps(root->left,sum,cur+root->val);
-            int r = ps(root->right,sum,cur+root->val);
-            return l+r;
-        }
-    }
-    int aps(TreeNode* root,int sum) {
-        if(!root)
-            return 0;
-        int le = ps(root->left,sum,0);
-        int ri = ps(root->right,sum,0);
-        int ro = ps(root,sum,0);
-        return le+ri+ro;
 
-    }
-    int pathSum(TreeNode* root, int sum) {
-        if(!root)
-            return 0;
-        int le = ps(root->left,sum,0);
-        int ri = ps(root->right,sum,0);
-        int ro = ps(root,sum,0);
-        return le+ri+ro;
-    }
+class Solution {
+	public:
+	//the first recursion: used to calu the pathNum from this root node to all child
+		int ps(TreeNode* root,int sum, int cur){
+			int l=0;
+			int r=0;
+			if(!root)
+				return 0;
+			else if((root->val + cur) == sum){
+				l = ps(root->left,sum,cur+root->val);
+				r = ps(root->right,sum,cur+root->val);
+				return 1+l+r;
+			}            
+			else{
+				l = ps(root->left,sum,cur+root->val);
+				r = ps(root->right,sum,cur+root->val);
+				return l+r;
+			}
+		}
+	//the second recursion:used to calu all the node treated as root, to sum up 
+		int aps(TreeNode* root,int sum) {
+			if(!root)
+				return 0;
+			else if(root->left==NULL && root->right==NULL)
+				return ps(root,sum,0);
+			else{
+				int le = aps(root->left,sum);
+				int ri = aps(root->right,sum);
+				int ro = ps(root,sum,0);
+				return le+ri+ro;
+			}
+
+		}
+		int pathSum(TreeNode* root, int sum) {
+			if(!root)
+				return 0;
+			return aps(root,sum);
+		}
 };
+
 int main(int argc, char* argv[])
 {
-  
+
 	return 0;
 }
 
