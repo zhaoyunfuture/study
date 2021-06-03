b0af809cbf25426d91f9cf040641e1da081afa56
zhaoyunfuture <zyfuture@qq.com>
2021-06-03 22:09:37 +0800

    add doxygen frame

---
+++ doxygen_frame/sub_mod/demo_app/main.hpp
@@ -0,0 +1,17 @@
+/**@file  main.hpp
+* @brief      describe in main 
+* @details    details in main
+* @author     Jacob
+* @date       21.6.3
+* @version     V1.0
+**********************************************************************************
+* @attention
+*  show time \n
+* @par log:
+* <table>
+* <tr><th>Date        <th>Version  <th>Author    <th>Description
+* <tr><td>21/06/     <td>1.0      <td>Jacob      <td>initial
+* </table>
+*
+**********************************************************************************
+*/
---
+++ doxygen_frame/top/main.hpp
@@ -0,0 +1,16 @@
+/**@mainpage
+ *\brief  doxygen demo
+ *\section part one
+ *  this is part one content...
+ *\section part two
+ *  this is part two content...
+ *
+ *
+ *
+ *
+ *<a href=../../demo_app/html/index.html>demo_app</a>
+ *
+ *
+ *
+ *
+ */
