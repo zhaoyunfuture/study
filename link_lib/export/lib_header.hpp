e5b61e358ad086364d1b6a2bf67ede374df5b5de
zyfuture <zyfuture@qq.com>
2021-09-10 17:04:03 +0800

    add link lib

---
+++ link_lib/export/lib_header.hpp
@@ -0,0 +1,9 @@
+#ifndef _LIB_HEARDER_
+#define _LIB_HEARDER_
+
+#include <string.h>
+#include <string>
+#include <iostream>
+int app_enter(int argc, char* argv[]) __attribute__((weak));
+int lib_main(int argc, char* argv[]);
+#endif
---
+++ link_lib/src/app.cpp
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <string>
+#include <iostream>
+ 
+#include <test_lib/lib_header.hpp>
+using namespace std;
+
+int app_enter(int argc, char* argv[]){
+    cout << "app world" << endl;
+    return 0;
+}
+
